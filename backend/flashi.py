93567f4f2ddf8a0e6feeec3a2c3c65aab508d5ff
Jordan Spooner <jordan.spooner16@imperial.ac.uk>
2018-01-27 19:16:49 +0000

    Finish PDF to text conversion

--- backend/flashi.py
+++ backend/flashi.py
@@ -1,61 +1,73 @@
 #!/usr/bin/env python3
 """
-Script to convert PDF into images and convert into text.
-
-IMPORTANT: This script requires imagemagick to be installed on your system.
+Script to convert PDF into images and request OCR
 """
 
-import http.client
-import subprocess
-import urllib.request
-import urllib.parse
-import urllib.error
-import base64
-import json
+import requests
+from wand.image import Image
+from wand.color import Color
 
 KEY = ''
 with open('api.key') as key:
     KEY = key.read().strip()
 
-URI_BASE = 'westcentralus.api.cognitive.microsoft.com'
+URI_BASE = 'https://westcentralus.api.cognitive.microsoft.com'
 
 HEADERS = {
     # Request headers.
-    'Content-Type': 'application/json',
+    'Content-Type': 'application/octet-stream',
     'Ocp-Apim-Subscription-Key': KEY,
 }
 
-PARAMS = urllib.parse.urlencode({
+PARAMS = {
     # Request parameters.
     'language': 'en',
-})
+}
 
 
-def ocr_request(body):
-    """ Make an OCR request to Microsoft cognitive services Vision API."""
+def pagify_and_ocr(fname):
+    num_pages = pagify_pdf(fname)
+    for page in range(num_pages):
+        image = ''
+        with open(fname+str(page+1)+'.png', 'rb') as file:
+            image = file.read()
+        ocr_request(image)
+
+
+def ocr_request(image):
+    """Make an OCR request to Microsoft cognitive services Vision API"""
     try:
-        # Execute the REST API call and get the response.
-        conn = http.client.HTTPSConnection(URI_BASE)
-        conn.request('POST', '/vision/v1.0/ocr?%s' % PARAMS, body, HEADERS)
-        response = conn.getresponse()
-        data = response.read()
-
-        # 'data' contains the JSON data. The following formats the JSON data for display.
-        parsed = json.loads(data)
-        print("Response:")
-        print(json.dumps(parsed, sort_keys=True, indent=2))
-        conn.close()
+        response = requests.post(url=URI_BASE+'/vision/v1.0/ocr',
+                                 headers=HEADERS,
+                                 params=PARAMS,
+                                 data=image)
+        data = response.json()
+        for region in data['regions']:
+            for line in region['lines']:
+                for word in line['words']:
+                    print(word['text'], end=' ')
+                print('')
+            print('')
+        print('')
 
     except Exception as err:
         print('An error occurred:')
         print(err)
 
 
-def pagify_pdf(uri):
-    """ Return individual images for each page of a PDF."""
-    ## convert -density 250 input.pdf output.jpg
-    subprocess.call()
-    
+def pagify_pdf(fname):
+    """Create a PNG image for each page of a PDF document"""
+    all_pages = Image(filename=fname+'.pdf', resolution=250)
+    count = 1
+    for page in all_pages.sequence:
+        img = Image(page)
+        img.format = 'png'
+        img.background_color = Color('white')
+        img.alpha_channel = 'remove'
+        img.save(filename=fname+str(count)+'.png')
+        count += 1
+    return count - 1
+
 
 if __name__ == '__main__':
-    ocr_request("{'url':'https://raw.githubusercontent.com/jordanspooner/first-year-notes/master/112.pdf'}")
+    pagify_and_ocr('test/140')
